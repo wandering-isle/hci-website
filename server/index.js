a44b8f5f6ce88267c70b8a5185c0be94246555ef
kaeberc <kaeberc@uw.edu>
2024-11-26 18:52:13 -0500

    Basic server set-up

--- server/index.js
+++ server/index.js
@@ -2,10 +2,29 @@ import express from'express';
 import cors from "cors";
 import fs from 'fs';
 
+import sdk from 'microsoft-cognitiveservices-speech-sdk' // audio->text
+
+const CORRECTIVE_CONTEXT = [{"role": "system", "content": "The following text may have incorrect words. Replace any words with more likely words based on context."}]
+
 const app = express();  // Server is instantiated
+
+// These options enable us to dump json payloads and define the return signal
+const corsOptions = {
+	origin: '*', 
+	credentials: true,
+	optionSuccessStatus: 200,
+  }
+  
+// Allow for large requests so audio and images can be sent to server
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit:'50mb'}));
+app.use(cors(corsOptions));
+
+
 
 // The actual post request for image and audio GPT processing
 app.post("/audio", async (req, res) => {
+
 	if (req.body.params.content) {
 		res.send({"content":"Test audio content"})
 	} else {
