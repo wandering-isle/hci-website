42de0163c08a6ffeb9463e929a3e31c53bb537b5
kaeberc <kaeberc@uw.edu>
2024-11-26 18:57:51 -0500

    I'm giving y'all access. use it well :/

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import sdk from 'microsoft-cognitiveservices-speech-sdk' // audio->text
 
 const CORRECTIVE_CONTEXT = [{"role": "system", "content": "The following text may have incorrect words. Replace any words with more likely words based on context."}]
 
+const counter = 0;
+
 const app = express();  // Server is instantiated
 
 // These options enable us to dump json payloads and define the return signal
@@ -21,6 +23,28 @@ app.use(express.urlencoded({limit:'50mb'}));
 app.use(cors(corsOptions));
 
 
+async function getCorrectiveResponse(text) {
+	const newMessages = [...TEXT_CONTEXT, 
+			{role: "user", content: text}
+	]
+	const response = await getGptResonse(newMessages);
+	return response.choices[0].message.content;
+}
+
+const speechConfig = sdk.SpeechConfig.fromSubscription("8186be822a5d456f9653c5f5303f055d", "eastus");
+
+async function audioToText(audioURL) {
+	counter += 1;
+	let cur_count = counter;
+	var b64_audio = req.body.params.audio.replace("data:audio/wav;base64,", "");
+	// Write the audio to a file
+	fs.writeFileSync("audio_out_"+cur_count+".wav", b64_audio, 'base64', function(err) {
+		console.log(err);
+	});
+	let audioConfig = sdk.AudioConfig.fromWavFileInput(fs.readFileSync("audio_out_temp.wav"));
+	let speechRecognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+	console.log("starting recog");
+}
 
 // The actual post request for image and audio GPT processing
 app.post("/audio", async (req, res) => {
